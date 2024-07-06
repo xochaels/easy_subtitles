28566c47c31b59590a9027484d12dd162fc76fa8
Rafael Simson Riston <rafaelsimsonriston@gmail.com>
2024-07-07 04:12:01 +0800

    change dir name in main py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import yaml
 import time
 
 
-from sub_download import (getUserToken, destroyUserToken, searchSubtitles,
+from scripts.sub_download import (getUserToken, destroyUserToken, searchSubtitles,
                           getSubtitlesInfo, downloadSubtitles, getUserInfo)
-from changetime import shift_subtitle
-from easy_translate import translate_subtitles
+from scripts.changetime import shift_subtitle
+from scripts.easy_translate import translate_subtitles
 
 def handle_remove_error(func, path, exc_info):
     print(f'Error removing {path}: {exc_info}')
@@ -50,18 +50,18 @@ def download_sub(downloaded_en_dir, id_tv_show, season, from_eps, to_eps, userna
 
 def sync_sub(downloaded_en_dir, change_time_dir, from_eps, to_eps, time_change, input_path_time):
     for eps in range(from_eps, to_eps + 1):
-        change_time_path = os.path.join(change_time_dir, f"episode_{eps}")
-        os.makedirs(change_time_path, exist_ok=True)
+        sync_subtitle_path = os.path.join(change_time_dir, f"episode_{eps}")
+        os.makedirs(sync_subtitle_path, exist_ok=True)
 
         download_sub_path = os.path.join(downloaded_en_dir, f"episode_{eps}")
         translate_dir_path = os.path.join(change_time_dir, f'episode_{eps}')
 
         if input_path_time == 1:
             shift_subtitle(os.path.join(download_sub_path, f'episode_{eps}.srt'),
-                           os.path.join(change_time_path, f'episode_{eps}.srt'), milliseconds=time_change)
+                           os.path.join(sync_subtitle_path, f'episode_{eps}.srt'), milliseconds=time_change)
         else:
             shift_subtitle(os.path.join(translate_dir_path, f'episode_{eps}.srt'),
-                           os.path.join(change_time_path, f'episode_{eps}.srt'), milliseconds=time_change)
+                           os.path.join(sync_subtitle_path, f'episode_{eps}.srt'), milliseconds=time_change)
     print(f'Done!! Check Your file in {change_time_dir}')
 
 
@@ -72,14 +72,14 @@ def translate(downloaded_en_dir, change_time_dir, translated_dir, from_eps, to_e
             os.makedirs(translate_dir_path, exist_ok=True)
 
         download_sub_path = os.path.join(downloaded_en_dir, f"episode_{eps}")
-        change_time_path = os.path.join(change_time_dir, f"episode_{eps}")
+        sync_subtitle_path = os.path.join(change_time_dir, f"episode_{eps}")
 
         if input_path_trans == 1:
             translate_subtitles(os.path.join(download_sub_path, f"episode_{eps}.srt"),
                                 os.path.join(translate_dir_path, f"episode_{eps}.srt"), target_language=target_language)
             time.sleep(3)
         else:
-            translate_subtitles(os.path.join(change_time_path, f"episode_{eps}.srt"),
+            translate_subtitles(os.path.join(sync_subtitle_path, f"episode_{eps}.srt"),
                                 os.path.join(translate_dir_path, f"episode_{eps}.srt"), target_language=target_language)
             time.sleep(3)
 
@@ -100,19 +100,19 @@ if __name__ == "__main__":
     target_language = conf['TRANSLATE']['TARGET_LANGUAGE']
     input_path_trans = conf['TRANSLATE']['INPUT_PATH']
 
-    translated_dir = os.path.join(os.getcwd(), 'srt_file', 'translated_srt')
-    downloaded_en_dir = os.path.join(os.getcwd(), 'srt_file', 'srt_downloaded_en')
-    change_time_dir = os.path.join(os.getcwd(), 'srt_file', 'change_time')
+    translated_dir = os.path.join(os.getcwd(), 'data_subtitles', 'translated_subtitle')
+    downloaded_en_dir = os.path.join(os.getcwd(), 'data_subtitles', 'downloaded_subtitle_en')
+    sync_subtitle = os.path.join(os.getcwd(), 'data_subtitles', 'sync_subtitle')
 
     if mode == 1:
         make_writable_and_remove(downloaded_en_dir)
         download_sub(downloaded_en_dir, id_tv_show, season, from_eps, to_eps, username, password)
 
     elif mode == 2:
-        make_writable_and_remove(change_time_dir)
-        sync_sub(downloaded_en_dir, change_time_dir, from_eps, to_eps, time_change, input_path_time)
+        make_writable_and_remove(sync_subtitle)
+        sync_sub(downloaded_en_dir, sync_subtitle, from_eps, to_eps, time_change, input_path_time)
 
     else:
         make_writable_and_remove(translated_dir)
-        translate(downloaded_en_dir, change_time_dir, translated_dir, from_eps, to_eps, target_language,
+        translate(downloaded_en_dir, sync_subtitle, translated_dir, from_eps, to_eps, target_language,
                   input_path_trans)
